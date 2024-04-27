5f1e4fbca4f7239a7100fc2af8a73ceaa305ad21
Ikhyeon Cho <tre0430@korea.ac.kr>
2024-04-27 21:53:58 +0900

    [Feat]: Update record_data.launch

---
+++ isr_m4_tools/record_isr_robot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rosbag record -O ~/Downloads/$1/isr_m4.bag \
+  /tf \
+  /tf_static \
+  /imu/data \
+  /odom/wheel
--- isr_m4_tools/record_proprioception.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rosbag record --lz4 -O ~/Downloads/$1/isr_m4.bag /tf /tf_static /imu/data /odom/wheel
